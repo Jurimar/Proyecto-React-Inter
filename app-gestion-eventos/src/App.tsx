8e34b1da178725dc6d1b7cb69600f6edd99aaf13
Fernanda-15 <fernandaqp15@gmail.com>
2024-10-13 18:39:08 -0600

    Commit prueba

--- app-gestion-eventos/src/App.tsx
+++ app-gestion-eventos/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-green-500 p-8 rounded shadow-lg">
-        <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+        <h1 className="text-4xl font-bold text-blue-500">Hola, Tailwind CSS!</h1>
       </div>
     </div>
   );
