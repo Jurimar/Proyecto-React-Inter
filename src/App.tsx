8d822ec458ed44a1f7dd76e31506bb35fff4ca73
Maikol Camareno Peña <mcamareno@coopeguanacaste.com>
2024-10-13 23:41:38 -0600

    Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LoginComponent from "./Layouts/LoginComponent";
+import NavBarComponent from "./Layouts/NavBarComponent";
 
 const App = () => {
   
   
   return (
     <div>
+      <NavBarComponent />
       <LoginComponent />
     </div>
       
